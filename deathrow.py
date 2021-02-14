ef8cf97c749df0eae4c2f3ad655dc3b194fa8c6e
Nissi Chima <nissichima@nissis-mbp.myfiosgateway.com>
2021-02-13 20:26:46 -0500

    filtered links using regular expressions

--- deathrow.py
+++ deathrow.py
@@ -1,10 +1,17 @@
 from requests_html HTMLSession
+import re
 
 
 session = HTMLSession()
 response = session.get("")
 
+links = list(response.html.absolute_links)
+pattern = re.compile(r'https:\/\/.+(dr_info)\/\w+\.html')
+filtered_links = list(filter(pattern.match, links))
 
 
 
 
+
+for url in filtered_links:
+	#do something for each link
