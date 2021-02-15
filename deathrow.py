268b2dd40ef78864a56dee2f2af07ecd2e503214
Nissi Chima <nissichima@Nissis-MacBook-Pro.local>
2021-02-15 14:50:33 -0500

    .....

--- deathrow.py
+++ deathrow.py
@@ -1,9 +1,9 @@
-from requests_html HTMLSession
+from requests_html import HTMLSession
 import re
 
 
 session = HTMLSession()
-response = session.get("")
+response = session.get("https://www.tdcj.texas.gov/death_row/dr_offenders_on_dr.html")
 
 links = list(response.html.absolute_links)
 pattern = re.compile(r'https:\/\/.+(dr_info)\/\w+\.html')
@@ -13,5 +13,7 @@ filtered_links = list(filter(pattern.match, links))
 
 
 
-for url in filtered_links:
-	#do something for each link
+for url in filtered_links[:2]:
+	response = session.get(url)
+	name = response.html.find('table')[0].find("tr")[0].text.split('\n')[1]
+	print(name)
